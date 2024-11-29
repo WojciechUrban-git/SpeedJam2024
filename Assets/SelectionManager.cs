164bd4a585604f09faf967e7e701ea3a4862b214
marqumax <116765876+marqumax@users.noreply.github.com>
2024-11-29 23:59:30 +0100

    Object Selection using raycast
    implemented UI at the center of the screen to facilitate object selection. All objects with "Selectable" tag can be selected. A raycast from the center of the screen is used to highlight the selected objects in yellow

---
+++ Assets/SelectionManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+
+    void Update()
+    {
+        // Deselect the previously selected object
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        // Cast a ray from the center of the screen
+        var ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+            }
+        }
+    }
+}
